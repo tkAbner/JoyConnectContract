ac5d59998525e082a415600e64afbdc003d41c61
DevSwayam <swayamkarle_ce_2020@ltce.in>
2024-10-27 21:53:30 +0530

    Final Formatted Confidential Erc20 Contract

--- contracts/EncryptedERC20.sol
+++ contracts/EncryptedERC20.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/access/Ownable2Step.sol";
 import "fhevm/gateway/GatewayCaller.sol";
 
 contract EncryptedERC20 is Ownable2Step, GatewayCaller {
+    // Events for Transfer, Approval, Mint, and Decryption
     event Transfer(address indexed from, address indexed to);
     event Approval(address indexed owner, address indexed spender);
     event Mint(address indexed to, uint64 amount);
@@ -17,97 +18,86 @@ contract EncryptedERC20 is Ownable2Step, GatewayCaller {
     string public _symbol;
     uint8 public constant decimals = 6;
 
-    // A mapping from address to an encrypted balance.
+    // Mappings for balances and allowances
     mapping(address => euint64) internal balances;
-
-    // A mapping of the form mapping(owner => mapping(spender => allowance)).
     mapping(address => mapping(address => euint64)) internal allowances;
 
+    // Constructor to set the token name, symbol, and owner
     constructor() Ownable(msg.sender) {
         _name = "Confidential USD";
         _symbol = "CUSD";
     }
 
-    // Sets the balance of the owner to the given encrypted balance.
+    // Mint function to create tokens and add to the owner's balance
     function mint(uint64 mintedAmount) public virtual onlyOwner {
-        balances[owner()] = TFHE.add(balances[owner()], mintedAmount); // overflow impossible because of next line
+        balances[owner()] = TFHE.add(balances[owner()], mintedAmount);
         TFHE.allow(balances[owner()], address(this));
         TFHE.allow(balances[owner()], owner());
-        _totalSupply = _totalSupply + mintedAmount;
+        _totalSupply += mintedAmount;
         emit Mint(owner(), mintedAmount);
     }
-
-    // Sets the balance of the owner to the given encrypted balance.
-    function _mint(uint64 mintedAmount) public virtual {
-        balances[msg.sender] = TFHE.add(balances[msg.sender], mintedAmount); // overflow impossible because of next line
+    
+    // Overloaded _mint function to allow encrypted token minting
+    function _mint(einput encryptedAmount, bytes calldata inputProof) public virtual onlyOwner {
+        balances[msg.sender] = TFHE.add(balances[msg.sender], TFHE.asEuint64(encryptedAmount, inputProof)); 
         TFHE.allow(balances[msg.sender], address(this));
         TFHE.allow(balances[msg.sender], owner());
         TFHE.allow(balances[msg.sender], msg.sender);
-        _totalSupply = _totalSupply + mintedAmount;
-        emit Mint(msg.sender, mintedAmount);
     }
 
-    // Transfers an encrypted amount from the message sender address to the `to` address.
+    // Transfer function for EOAs using encrypted inputs
     function transfer(address to, einput encryptedAmount, bytes calldata inputProof) public virtual returns (bool) {
         transfer(to, TFHE.asEuint64(encryptedAmount, inputProof));
         return true;
     }
 
-    // Transfers an amount from the message sender address to the `to` address.
+    // Transfer function for contracts
     function transfer(address to, euint64 amount) public virtual returns (bool) {
         require(TFHE.isSenderAllowed(amount));
-        // makes sure the owner has enough tokens
         ebool canTransfer = TFHE.le(amount, balances[msg.sender]);
         _transfer(msg.sender, to, amount, canTransfer);
         return true;
     }
 
-    // Returns the balance handle of the caller.
+    // Retrieves the balance handle of a specified wallet
     function balanceOf(address wallet) public view virtual returns (euint64) {
         return balances[wallet];
     }
 
-    // Sets the `encryptedAmount` as the allowance of `spender` over the caller's tokens.
+    // Approve function for EOAs with encrypted inputs
     function approve(address spender, einput encryptedAmount, bytes calldata inputProof) public virtual returns (bool) {
         approve(spender, TFHE.asEuint64(encryptedAmount, inputProof));
         return true;
     }
 
-    // Sets the `amount` as the allowance of `spender` over the caller's tokens.
+    // Approve function for contracts
     function approve(address spender, euint64 amount) public virtual returns (bool) {
         require(TFHE.isSenderAllowed(amount));
-        address owner = msg.sender;
-        _approve(owner, spender, amount);
-        emit Approval(owner, spender);
+        _approve(msg.sender, spender, amount);
+        emit Approval(msg.sender, spender);
         return true;
     }
 
-    // Returns the remaining number of tokens that `spender` is allowed to spend
-    // on behalf of the caller.
+    // Retrieves the allowance handle for a spender
     function allowance(address owner, address spender) public view virtual returns (euint64) {
         return _allowance(owner, spender);
     }
 
-    // Transfers `encryptedAmount` tokens using the caller's allowance.
-    function transferFrom(
-        address from,
-        address to,
-        einput encryptedAmount,
-        bytes calldata inputProof
-    ) public virtual returns (bool) {
+    // TransferFrom function for EOAs with encrypted inputs
+    function transferFrom(address from, address to, einput encryptedAmount, bytes calldata inputProof) public virtual returns (bool) {
         transferFrom(from, to, TFHE.asEuint64(encryptedAmount, inputProof));
         return true;
     }
 
-    // Transfers `amount` tokens using the caller's allowance.
+    // TransferFrom function for contracts
     function transferFrom(address from, address to, euint64 amount) public virtual returns (bool) {
         require(TFHE.isSenderAllowed(amount));
-        address spender = msg.sender;
-        ebool isTransferable = _updateAllowance(from, spender, amount);
+        ebool isTransferable = _updateAllowance(from, msg.sender, amount);
         _transfer(from, to, amount, isTransferable);
         return true;
     }
 
+    // Internal function to handle allowance approvals
     function _approve(address owner, address spender, euint64 amount) internal virtual {
         allowances[owner][spender] = amount;
         TFHE.allow(amount, address(this));
@@ -115,36 +105,38 @@ contract EncryptedERC20 is Ownable2Step, GatewayCaller {
         TFHE.allow(amount, spender);
     }
 
+    // Internal function to retrieve an allowance handle
     function _allowance(address owner, address spender) internal view virtual returns (euint64) {
         return allowances[owner][spender];
     }
 
+    // Internal function to update an allowance securely
     function _updateAllowance(address owner, address spender, euint64 amount) internal virtual returns (ebool) {
         euint64 currentAllowance = _allowance(owner, spender);
-        // makes sure the allowance suffices
         ebool allowedTransfer = TFHE.le(amount, currentAllowance);
-        // makes sure the owner has enough tokens
         ebool canTransfer = TFHE.le(amount, balances[owner]);
         ebool isTransferable = TFHE.and(canTransfer, allowedTransfer);
         _approve(owner, spender, TFHE.select(isTransferable, TFHE.sub(currentAllowance, amount), currentAllowance));
         return isTransferable;
     }
 
-    // Transfers an encrypted amount.
+    // Internal transfer function for encrypted token transfer
     function _transfer(address from, address to, euint64 amount, ebool isTransferable) internal virtual {
-        // Add to the balance of `to` and subract from the balance of `from`.
         euint64 transferValue = TFHE.select(isTransferable, amount, TFHE.asEuint64(0));
         euint64 newBalanceTo = TFHE.add(balances[to], transferValue);
         balances[to] = newBalanceTo;
         TFHE.allow(newBalanceTo, address(this));
         TFHE.allow(newBalanceTo, to);
+
         euint64 newBalanceFrom = TFHE.sub(balances[from], transferValue);
         balances[from] = newBalanceFrom;
         TFHE.allow(newBalanceFrom, address(this));
         TFHE.allow(newBalanceFrom, from);
+
         emit Transfer(from, to);
     }
 
+    // Owner-only function to request decryption of a user's balance
     function requestUserBalanceDecryption(address user) public onlyOwner returns (uint256) {
         euint64 encryptedBalance = balances[user];
         TFHE.allow(encryptedBalance, address(this));
@@ -163,6 +155,7 @@ contract EncryptedERC20 is Ownable2Step, GatewayCaller {
         return requestId;
     }
 
+    // Callback function to handle decrypted balance for a user
     function onDecryptionCallback(uint256 requestId, uint64 decryptedAmount) public onlyGateway returns (bool) {
         address[] memory params = getParamsAddress(requestId);
         emit UserBalanceDecrypted(params[0], decryptedAmount);
